2ae36a4458a8221f3041dd1d1996e751e567c037
gopal-sakshi <gopal612@gmail.com>
2022-12-16 13:18:23 +0530

    ss

---
+++ exports_vs_module.exports/5_sideEffect.js
@@ -0,0 +1,3 @@
+console.log(`5 loaded`);                    // See webpack44 sideEffects
+                                                // 5_sideEffect.js ==> performing some externally-visible behaviour
+exports.something = 'somethingExported';
---
+++ exports_vs_module.exports/using5.js
@@ -0,0 +1,2 @@
+const five = require('./5_sideEffect');
+console.log(five.something);
